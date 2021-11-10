7c0d8cf7d03b2aad16b6c82fa07a3bef1b2439bd
Cory Evans <cory.evans@mailbox.org>
2021-11-11 10:34:42 +1300

    filter by lat lon

--- routes/core.go
+++ routes/core.go
@@ -38,6 +38,8 @@ func UploadTracks(w http.ResponseWriter, r *http.Request) {
 
 func GetTracks(w http.ResponseWriter, r *http.Request) {
 	limitStr := r.URL.Query().Get("limit")
+	lat := r.URL.Query().Get("lat")
+	lon := r.URL.Query().Get("lon")
 
 	limit, err := strconv.Atoi(limitStr)
 	if err != nil {
@@ -48,8 +50,23 @@ func GetTracks(w http.ResponseWriter, r *http.Request) {
 		limit = 1000
 	}
 
-	var tracks []models.Track
-	result := database.DB.Limit(limit).Find(&tracks)
+	var tracks []struct {
+		models.Track
+		Distance float64 `json:"distance"`
+	}
+
+	// result := database.DB.Limit(limit).Find(&tracks)
+	result := database.DB.Raw(`
+SELECT
+	calculate_distance(?, ?, latlon.ll[1], latlon.ll[0], 'K') as distance,
+	id, description, status, shape_length, object_type_description, geometry
+FROM (
+	SELECT geometry::POLYGON::POINT as ll, id, description, status, shape_length, object_type_description, geometry
+	FROM tracks
+) as latlon
+ORDER BY distance
+LIMIT ?;
+	`, lat, lon, limit).Scan(&tracks)
 
 	if result.Error != nil {
 		w.WriteHeader(http.StatusInternalServerError)
